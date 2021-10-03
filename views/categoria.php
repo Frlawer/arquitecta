bc0c13a0edc0033143e290676f7dd2797287ce81
frlawer <frlawer@gmail.com>
2021-10-03 10:19:40 -0300

    refactorizado la vista. falta mejorar links

--- views/categoria.php
+++ views/categoria.php
@@ -3,28 +3,30 @@ require_once('./clases/Img.php');
 require_once('./clases/Categoria.php'); 
 require_once('./clases/Proyecto.php'); 
 // si existe el numero de proyecto muestro la galería.
-if (isset($_GET['n']) == 'viv01'):
+if (isset($_GET['n'])):
 
 	$proyectoId = $_GET['n'];
+	$categoriaId = $_GET['id'];
 	
-	$img = new Img();
-	$img->select1($proyectoId);
-	$datos = $img->rows;
-
 	// solicito datos proyecto
 	$proyecto = new Proyecto();
-	$proyecto->getProyecto($proyectoId);
-	$datosP = $proyecto->rows[0];
+	$proyecto->getProyecto($proyectoId, $categoriaId);
+	$proyectoData = $proyecto->rows[0];
+
+	$img = new Img();
+	$img->getImgs( $categoriaId, $proyectoId);
+	$imgData = $img->rows;
+
 	?>
 	<!-- Four -->
 	<section class="wrapper style1 align-center" id="categoria">
 		<div class="inner">
-			<h2><?php echo $datosP['nombre'] ?></h2>
-			<blockquote><?php echo $datosP['resenia'] ?></blockquote>
+			<h2><?php echo $proyectoData['nombre'] ?></h2>
+			<blockquote><?php echo $proyectoData['resenia'] ?></blockquote>
 		</div>
-		<div class="gallery style1 lightbox onload-fade-in">
+		<div class="gallery style2 small lightbox onload-fade-in">
 			<?php
-			foreach ($datos as $key => $value) { ?>
+			foreach ($imgData as $key => $value) { ?>
 			<article>
 				<a href="<?php echo $value['url']?>" class="image">
 					<img src="<?php echo $value['url']?>" alt="Alternate text" />
@@ -38,41 +40,39 @@ if (isset($_GET['n']) == 'viv01'):
 	
 <?php
 // Sino muestro la lista de proyectos
-elseif(isset($_GET['id']) == 1): 
-	$lista = $_GET['id'];
-	$datos = new Img();
-	$datos->selectCategoria($lista);
-	$datosImg = $datos->rows;
+elseif(isset($_GET['id'])): 
+	$idCategoria = $_GET['id'];
 
-	$imgLista = new Img();
-	$imgLista->select1($lista);
-	$datos = $imgLista->rows;
+	$proyecto = new Proyecto();
+    $proyecto->getProyectos($idCategoria);
+    $proyectoData = $proyecto->rows;
 
-	
+	$categoria = new Categoria();
+    $categoria->nombreCat($idCategoria);
+
 	?>
 	<!-- Four -->
 	<section class="wrapper style1 align-center" id="categoria">
 		<div class="inner">
-			<h2>Viviendas Unifamiliares</h2>
+			<h2><?php echo ucfirst($categoria->rows[0]['nombre']) ?></h2>
 			<p>Selecciona para ver más fotos</p>
 		</div>
 		<div class="items style1 small onload-fade-in">
-			<?php foreach ($datosImg as $key => $value) : ?>
+			<?php foreach ($proyectoData as $key => $value) : ?>
+                <?php 
+                // creo otro objeto con 1 solo llamado a la imagen principal
+                $img = new Img();
+                $img->getImg($value['categoria_id'], $value['subCategoria']);
+                    ?>
 				<section>
 					<a href="?view=categoria&id=<?php echo $value['categoria_id'];?>&n=<?php echo $value['subCategoria']; ?>">
-						<img src="<?php echo $value['url']?>" width="100%" alt="<?php echo $value['nombre'] ?>" />
+						<img src="<?php echo $img->rows[0]['url']?>" width="100%" alt="<?php echo $value['nombre'] ?>" />
 					</a>
-				<?php
-				// Solicito datos de proyecto
-				$proyectoG = new Proyecto();
-				$proyectoG->getProyectoCat($value['subCategoria']);
-				$datosG = $proyectoG->rows[0];
-				?>
 					<h3>
-				<?php echo $datosG['nombre']; ?>
+				<?php echo $value['nombre']; ?>
 					</h3>
 					<p>
-						<?php echo mb_substr($datosG['resenia'], 0, 50, 'UTF-8')."..."; ?>
+						<?php echo mb_substr($value['resenia'], 0, 50, 'UTF-8')."..."; ?>
 					</p>
 				</section>
 			<?php endforeach; ?>
