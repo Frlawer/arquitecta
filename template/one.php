4f8b260723bb2114f1640df25351f4b7f5c06760
frlawer <frlawer@gmail.com>
2021-10-15 23:16:24 -0300

    introducir h2 y span en class .caja

--- template/one.php
+++ template/one.php
@@ -1,33 +1,33 @@
 <section class="wrapper">
-    <div id="card-slider" class="splide" data-splide='{"type":"fade","cover": "true","heightRatio":"0.5", "width":"100vw"}'>
+    <div id="card-slider" class="splide" data-splide='{"type":"fade","cover": "true","heightRatio":"0.5", "width":"100vw","autoplay":"true"}'>
         <div class="splide__track">
             <ul class="splide__list">
                 <li class="splide__slide">
                     <img src="images/slider/slider01.jpg"/>
-                    <h2>VIVIENDAS UNIFAMILIARES</h2>
                     <div class="caja">
-                        VER MÁS >
+                        <h2>VIVIENDAS UNIFAMILIARES</h2>
+                        <span>VER MÁS ></span>
                     </div>
                 </li>
                 <li class="splide__slide">
                     <img src="images/slider/slider02.jpg"/>
-                    <h2>HOTELES</h2>
                     <div class="caja">
-                        VER MÁS >
+                        <h2>HOTELES</h2>
+                        <span>VER MÁS ></span>
                     </div>
                 </li>
                 <li class="splide__slide">
                     <img src="images/slider/slider04.jpg"/>
-                    <h2>INDUSTRIA</h2>
                     <div class="caja">
-                        VER MÁS >
+                        <h2>INDUSTRIA</h2>
+                        <span>VER MÁS ></span>
                     </div>
                 </li>
                 <li class="splide__slide">
                     <img src="images/slider/slider05.jpg"/>
-                    <h2>OBRAS VARIAS</h2>
                     <div class="caja">
-                        VER MÁS >
+                        <h2>OBRAS VARIAS</h2>
+                        <span>VER MÁS ></span>
                     </div>
                 </li>
             </ul>
