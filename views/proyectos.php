44a42ea94797616e156ace4c9bff7ffb3d1fa3a5
frlawer <frlawer@gmail.com>
2021-10-18 20:47:00 -0300

    borrado el texto del caption, solo va titulo

--- views/proyectos.php
+++ views/proyectos.php
@@ -10,7 +10,6 @@
 			</a>
 			<div class="caption">
 				<h3>Diseño de Viviendas Unifamiliares</h3>
-				<p>Nos especializamos en la personalización de proyectos, diseñando cada detalle.</p>
 				<ul class="actions fixed">
 					<li><a href="?view=categoria&id=1"><span class="button small">Detalles</span></a></li>
 				</ul>
@@ -22,7 +21,6 @@
 			</a>
 			<div class="caption">
 				<h3>Hotelería</h3>
-				<p>Evaluamos el seguimiento y control de los avances, generando ahorros a nuestros clientes.</p>
 				<ul class="actions fixed">
 					<li><a href="?view=categoria&id=2"><span class="button small">Detalles</span></a></li>
 				</ul>
@@ -34,7 +32,6 @@
 			</a>
 			<div class="caption">
 				<h3>Industria</h3>
-				<p>Construimos con altos standard de calidad, con grupos especializados para cada rubro.</p>
 				<ul class="actions fixed">
 					<li><a href="?view=categoria&id=3"><span class="button small">Detalles</span></a></li>
 				</ul>
