dce338c5aa427da8cae955f1bfa985e2b7682891
frlawer <frlawer@gmail.com>
2021-10-19 10:04:04 -0300

    cambiados las imagenes del slider principal

--- template/one.php
+++ template/one.php
@@ -10,21 +10,21 @@
                     </div>
                 </li>
                 <li class="splide__slide">
-                    <img src="images/slider/slider02.jpg"/>
+                    <img src="images/slider/slider08.jpg"/>
                     <div class="caja">
                         <h2>HOTELES</h2>
                         <span>VER MÁS ></span>
                     </div>
                 </li>
                 <li class="splide__slide">
-                    <img src="images/slider/slider04.jpg"/>
+                    <img src="images/slider/slider12.jpg"/>
                     <div class="caja">
                         <h2>INDUSTRIA</h2>
                         <span>VER MÁS ></span>
                     </div>
                 </li>
                 <li class="splide__slide">
-                    <img src="images/slider/slider05.jpg"/>
+                    <img src="images/slider/slider13.jpg"/>
                     <div class="caja">
                         <h2>OBRAS EN EJECUCIÓN</h2>
                         <span>VER MÁS ></span>
