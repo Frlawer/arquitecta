3a3c9438af349ab8d9cee7d6d6ef2bf300a57ff5
frlawer <frlawer@gmail.com>
2021-10-21 10:24:46 -0300

    quitado texto de cada categoria en home

--- template/hoteleria.php
+++ template/hoteleria.php
@@ -3,7 +3,6 @@
 <section class="spotlight style1 orient-right content-align-left image-position-left onscroll-image-fade-in" id="hoteles">
 		<div class="content">
 			<h2>HOTELERÍA</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.</p>
 			<ul class="actions stacked">
 				<li><a href="?view=categoria&id=2" class="button">LEER MÁS</a></li>
 			</ul>
--- template/industrial.php
+++ template/industrial.php
@@ -2,7 +2,6 @@
 <section class="spotlight style4" id="industrial">
 	<div class="content">
 		<h2>INDUSTRIAL</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.</p>
 		<ul class="actions stacked">
 			<li><a href="?view=categoria&id=3" class="button">LEER MÁS</a></li>
 		</ul>
