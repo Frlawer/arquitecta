cd40a98945b72b7bd0c521d4baae4744e5f4e6a1
frlawer <frlawer@gmail.com>
2021-10-24 16:51:52 -0300

    up

--- template/head.php
+++ template/head.php
@@ -1,18 +1,12 @@
 <!DOCTYPE HTML>
-<!--
-	Story by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
 	<head>
 		<title>Borgo Arquitectura</title>
-	<meta charset="utf-8" />
+		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="./assets/css/main.css" />
 		<link rel="icon" href="./images/Isologotipo-negro.png" />
 		<noscript><link rel="stylesheet" href="./assets/css/noscript.css" /></noscript>
-		<!-- Link to the file hosted on your server, -->
 		<link rel="stylesheet" href="./assets/splide/dist/css/splide.min.css">
 	</head>
 	<body class="is-preload">
@@ -31,12 +25,12 @@
 					<div class="col-9 col-10-large col-12-medium" id="nav">
 						<ul>
 							<li><a href="/">INICIO</a></li>
-							<li><a href="/#nosotros" class="smooth-scroll">ESTUDIO</a></li>
+							<li><a href="#nosotros" class="smooth-scroll">ESTUDIO</a></li>
 							<li><a href="?view=proyectos" class="smooth-scroll">PROYECTOS</a></li>
-							<li><a href="/#industrial" class="smooth-scroll">INDUSTRIAL</a></li>
-							<li><a href="/#viviendas" class="smooth-scroll">VIVIENDAS</a></li>
-							<li><a href="/#hoteles" class="smooth-scroll">HOTELES</a></li>
-							<li><a href="/#contacto" class="smooth-scroll">CONTACTO</a></li>
+							<li><a href="#industrial" class="smooth-scroll">INDUSTRIAL</a></li>
+							<li><a href="#viviendas" class="smooth-scroll">VIVIENDAS</a></li>
+							<li><a href="#hoteles" class="smooth-scroll">HOTELES</a></li>
+							<li><a href="#contacto" class="smooth-scroll">CONTACTO</a></li>
 							<li><a href="https://wa.me/5492944394034?text=Hola%20Julieta%20Borgonobo,%20tome%20tu%20contacto%20desde%20la%20web%20de%20borgoarquitectura.com.ar" class="icon brands style2 fa-whatsapp"><span class="label">Whatsapp</span></a></li>
 						</ul>
 					</div>
